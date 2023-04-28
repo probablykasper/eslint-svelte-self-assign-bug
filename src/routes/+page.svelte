601419d5fc08cd2b1ceb1f5383f004496604746b
Kasper <kasperkh.kh@gmail.com>
2023-04-29 00:18:07 +0200

    Add self-assignment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script lang="ts">
+  let x = new Date();
+  setTimeout(() => {
+    x.setFullYear(2020);
+    x = x;
+  }, 1000);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
